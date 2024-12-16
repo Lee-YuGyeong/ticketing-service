a764cf6ce984bac3ee69e9ec1a6695df1f0180d0
YuGyeong <lyg64522@naver.com>
2024-12-16 16:08:46 +0900

    chore: 프로젝트 초기 세팅
    - Spring Boot 프로젝트 생성
    - 의존성 추가: Spring Web, JPA, Lombok
    - application.yml 기본 설정 추가
    - .gitignore 작성

---
+++ src/test/java/com/yugyeong/ticketing_service/TicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yugyeong.ticketing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
